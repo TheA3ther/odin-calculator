16fe969474590a2f7710ca10d4f8c4de4d608b29
TheA3ther <carlosroycanoperez@gmail.com>
2024-10-03 14:38:23 +0800

    Recover lost local changes due to accidental repo deletion

---
+++ script.js
@@ -0,0 +1,34 @@
+
+//operation functions
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+//main operate function
+
+function operate(a, b, operator){
+    switch (operator){
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '×':
+            return multiply(a, b);
+        case '÷':
+            return divide(a, b);
+    }
+}
+
