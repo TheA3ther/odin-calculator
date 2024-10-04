546c9c4469b6c0f2fbad92f0e81f575d53367695
TheA3ther <carlosroycanoperez@gmail.com>
2024-10-04 10:56:24 +0800

    Add UpdateDisplay functionality

--- click-module.js
+++ click-module.js
@@ -4,22 +4,23 @@ import * as DisplayModule from './display-module.js';
 
 //variables
 const MAXLENGTH = 20;
-let operandOne = undefined;
-let operandTwo = undefined;
+let calculatedFloat = undefined;
+let opCurrentAsFloat = undefined;
+let operator = undefined;
 let opCurrentArray = [];
-let opHistoryString = '';
 let isReplaceable = false;
 let isNegative = false;
 
 //click button function
 export function clickButton(event){
     let target = event.target;
+    let canUpdateOpHistory = false;
     if (target.classList.contains("number")) clickNumber(target);
     if (target.classList.contains("operator")) clickOperator(target);
     if (target.id == "equals") clickEquals(target);
     if (target.id == "decimal") clickDecimal(target);
     if (target.id == "sign") clickSign(target);
-    DisplayModule.UpdateDisplays();
+    DisplayModule.UpdateDisplays(opCurrentArray, isNegative, operator, canUpdateOpHistory);
 }
 
 //specific button functions
@@ -50,4 +51,9 @@ function clickDecimal(target){
 
 function clickSign(target){
 
+}
+
+//helper functions
+function opCurrentArrayToFloat(){
+    return parseFloat(opCurrentArray.join('')) * (isNegative ? -1 : 1);
 }
--- display-module.js
+++ display-module.js
@@ -1,11 +1,10 @@
-//variables
-let opHistoryDisplay = '';
-let opCurrentDisplay = '';
-
 //query selectors
 const opCurrent = document.querySelector("#op-current");
 const opHistory = document.querySelector("#op-history");
 
-export function UpdateDisplays(){
-    
+export function UpdateDisplays(opCurrentArray, isNegative, operator, canUpdateOpHistory){
+    let opCurrentString = opCurrentArray.join('');
+    let opHistoryString = (isNegative ? '- ' : '') + opCurrentString + (operator !== undefined ? (" " + operator) : "");
+    opCurrent.textContent = opCurrentString;
+    if (canUpdateOpHistory) opHistory.textContent = opHistoryString;
 }
